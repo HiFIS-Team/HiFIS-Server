26d2e6cfb7319c38f8c6ebf6130a1d775feaed3c
김은후 <eunhoo0903@naver.com>
2026-05-12 19:09:43 +0900

    🔧 Alembic env.py에 PTApplication 모델 import 추가

--- alembic/env.py
+++ alembic/env.py
@@ -15,6 +15,7 @@ from app.models import reservation as _reservation  # noqa: F401
 from app.models import membership_pass as _membership_pass  # noqa: F401
 from app.models import pt_pass as _pt_pass  # noqa: F401 
 from app.models import member as _member  # noqa: F401
+from app.models import pt_application as _pt_application  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
